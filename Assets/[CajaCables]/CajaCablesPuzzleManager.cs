01c5631f75dcbc6fdd69a56683dcccf8bc65af1d
hayate <davidlopez.gandia@gmail.com>
2024-01-08 15:33:42 +0100

    Cables funcionando en VR y boton puerta funcional

---
+++ Assets/[CajaCables]/CajaCablesPuzzleManager.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CajaCablesPuzzleManager : MonoBehaviour
+{
+    [SerializeField] GameObject caja;
+
+    public UnityEvent OnPuzzleCompleted;
+
+    int cables_count = 0;
+
+    public void CableConnected()
+    {
+        cables_count++;
+        Debug.Log(cables_count);
+        CheckPuzzle();
+    }
+
+    void CheckPuzzle()
+    {
+        if (cables_count == 3)
+        {
+            //Puzzle completed
+            Debug.Log("All cables connected!");
+            OnPuzzleCompleted?.Invoke();
+        }
+    }
+    public void AbrirCaja(bool abrir)
+    {
+        caja.GetComponent<Animator>().SetBool("opened", abrir);
+    }
+
+}
